b405dbe9d4f09617f04e050e2fb4e0de9bd23208
JonDev22 <jonnydeveloper22@gmail.com>
2026-01-07 12:42:16 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,65 +4,16 @@ import AuthProvider from "./contexts/AuthProvider";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./clients/queryClient";
 import MusicPage from "./sites/music/MusicPage";
-import { Fragment, Suspense, useState } from "react";
+import { Suspense, useState } from "react";
 import LoadingComponent from "./utils/LoadingComponent";
 import Home from "./sites/home/Home";
 import AuthComponents from "./utils/AuthComponents";
-import ContactPage from "./sites/contact/ContactPage";
+// import ContactPage from "./sites/contact/ContactPage";
 import ValuesPage from "./sites/values/ValuesPage";
 import PlanningPage from "./sites/planning/PlanningPage";
-import type { PageType } from "./types/PageType";
-import { Tab, TabGroup, TabList, TabPanel, TabPanels } from "@headlessui/react";
-
-import {
-    Bars3Icon,
-    XMarkIcon,
-    UserCircleIcon,
-    MusicalNoteIcon,
-} from "@heroicons/react/24/outline";
-
-// interface RouteConfig {
-//     path: PageType;
-//     render: () => React.ReactNode;
-//     authOnly: boolean;
-// }
+import { TabGroup, TabPanel, TabPanels } from "@headlessui/react";
 
 function App() {
-    const [activePage, setActivePage] = useState<PageType>("home");
-
-    // const routes: RouteConfig[] = [
-    //     {
-    //         path: "home",
-    //         render: () => <Home />,
-    //         authOnly: false,
-    //     },
-    //     {
-    //         path: "music",
-    //         render: () => <MusicPage />,
-    //         authOnly: true,
-    //     },
-    //     {
-    //         path: "contact",
-    //         render: () => <ContactPage />,
-    //         authOnly: false,
-    //     },
-    //     {
-    //         path: "planning",
-    //         render: () => <PlanningPage />,
-    //         authOnly: true,
-    //     },
-    //     {
-    //         path: "values",
-    //         render: () => <ValuesPage />,
-    //         authOnly: false,
-    //     },
-    //     {
-    //         path: "login",
-    //         render: () => <Login />,
-    //         authOnly: false,
-    //     },
-    // ];
-
     const [selectedPanel, setSelectedPanel] = useState<number>(0);
 
     const tabs = [
@@ -102,26 +53,9 @@ function App() {
                         onChange={(e: number) => setSelectedPanel(e)}
                         selectedIndex={selectedPanel}
                     >
-                        <TabList className="items-center flex gap-2 p-2">
-                            {tabs.map((tab) => (
-                                <Tab as={Fragment} key={tab.name}>
-                                    {({ hover, selected }) => (
-                                        <p
-                                            className={`px-4 py-2 rounded-lg bg-blue-600 text-white ${hover && "hover:bg-blue-700 hover:cursor-pointer"} ${selected && "bg-blue-900"}`}
-                                        >
-                                            {tab.name}
-                                        </p>
-                                    )}
-                                </Tab>
-                            ))}
-                            <Tab as={Fragment}>
-                                {({ hover, selected }) => (
-                                    <UserCircleIcon
-                                        className={`rounded-lg text-black ${hover && "hover:text-gray-500 hover:cursor-pointer"} ${selected && "text-gray-500"} h-10 w-10`}
-                                    />
-                                )}
-                            </Tab>
-                        </TabList>
+                        <Navbar
+                            tabs={tabs.map((tab) => ({ name: tab.name }))}
+                        />
 
                         <TabPanels>
                             {tabs.map((tab) => (
